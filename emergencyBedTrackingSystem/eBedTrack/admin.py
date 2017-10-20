0505712a46546fcb677239800bcd6d9449b9d197
Jantz021991 <djantz@unomaha.edu>
2017-10-20 10:13:07 -0500

    Modifying models'

--- emergencyBedTrackingSystem/eBedTrack/admin.py
+++ emergencyBedTrackingSystem/eBedTrack/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import patient,hospital, bedInfo, nurseBed, administrator, nurse
 
 # Register your models here.
+class PatientList(admin.ModelAdmin):
+    list_display = ('patientId','firstName','lastName','sex' )
+    list_filter = ('patientId','firstName','lastName','sex' )
+    search_fields =('firstName','lastName')
+    ordering = ['firstName']
+
+
+class HospitalList(admin.ModelAdmin):
+    list_display = ('hospitalId', 'hospitalName', 'hospitalAddress', 'hospitalPhoneNo')
+    list_filter = ('hospitalId', 'hospitalName', 'hospitalAddress', 'hospitalPhoneNo')
+    search_fields = ('hospitalName', 'hospitalAddress', 'hospitalPhoneNo')
+    ordering = ['hospitalName']
+
+
+class NurseList(admin.ModelAdmin):
+    list_display = ('nurseId', 'nFirstName', 'nLastName', 'hospitalId')
+    list_filter = ('nurseId', 'nFirstName', 'nLastName', 'hospitalId')
+    search_fields = ( 'nFirstName', 'nLastName', 'hospitalId')
+    ordering = ['nFirstName']
+
+class bedInfoList(admin.ModelAdmin):
+    list_display = ('bedId', 'bedType', 'hospitalId')
+    list_filter = ('bedId', 'bedType',  'hospitalId')
+    search_fields = ('bedId', 'bedType',  'hospitalId')
+    ordering = ['bedType']
+
+class administratorList(admin.ModelAdmin):
+    list_display = ('adminId', 'adminName')
+    list_filter = ('adminId', 'adminName')
+    search_fields = ('adminId', 'adminName')
+    ordering = ['adminName']
+
+class nurseBedList(admin.ModelAdmin):
+   list_display = ('nurseBedId', 'nurseId', 'bedId')
+   list_filter = ('nurseBedId', 'nurseId', 'bedId')
+   search_fields = ('nurseBedId', 'nurseId', 'bedId')
+   ordering = ['nurseBedId']
+
+admin.site.register(patient, PatientList)
+admin.site.register(hospital, HospitalList)
+admin.site.register(nurse, NurseList)
+admin.site.register(bedInfo, bedInfoList)
+admin.site.register(administrator, administratorList)
+admin.site.register(nurseBed, nurseBedList)
+
--- emergencyBedTrackingSystem/eBedTrack/models.py
+++ emergencyBedTrackingSystem/eBedTrack/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class patient(models.Model):
+    patientId =  models.AutoField(primary_key=True)
+    firstName = models.CharField("Patient's first name", max_length=100 ,default='Patients first name')
+    lastName = models.CharField("Patient's last name",max_length=100,default='Patients last name')
+    sex = models.CharField("Patient's sex",max_length=20,default='Male/Female/Other')
+    pTimeOfAdmission = models.DateTimeField(default=timezone.now)
+    pCondition = models.CharField(max_length= 50,default='Patients initial condition')
+    pBedType = models.CharField(max_length= 50)
+    pModeOfArrival = models.CharField(max_length=50)
+    pAge = models.IntegerField()
+    pBirthDate = models.DateTimeField(default=timezone.now)
+    pPhoneNo = models.CharField(max_length=12)
+    injuries =models.CharField(max_length= 50)
+    deposition = models.CharField(max_length=50)
+    pTimeOfSurgery = models.DateTimeField (default=timezone.now)
+    kinName = models.CharField(max_length=150)
+    relation = models.CharField(max_length=50)
+    pTimeOfDeath = models.DateTimeField(default=timezone.now)
+    nurseId = models.ForeignKey(nurse, on_delete=models.CASCADE)
+    bedId = models.ForeignKey(bedInfo, on_delete=models.CASCADE)
+
+    def created(self):
+        self.pTimeOfAdmission = timezone.now()
+        self.save()
+
+    def updated(self):
+        self.pTimeOfAdmission = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.patientId
+
+class nurse(models.Model):
+    nurseId = models.AutoField(primary_key=True)
+    nFirstName = models.CharField("Nurse's first name",max_length=100,default='Nurse first name')
+    nLastName = models.CharField("Nurse's first name",max_length=100,default='Nurse last name')
+    nAddress = models.CharField(max_length=250)
+    nPhoneNo = models.CharField(max_length=12)
+    hospitalId = models.ForeignKey(hospital, on_delete=models.CASCADE)
+    adminId = models.ForeignKey(administrator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nurseId
+
+class bedInfo(models.Model):
+    bedId = models.AutoField(primary_key=True)
+    bedType = models.CharField(max_length=50)
+    #hospitalId = models.ForeignKey(hospital, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.bedId
+
+class hospital(models.Model):
+    hospitalId = models.AutoField(primary_key=True)
+    hospitalName = models.CharField(max_length=100)
+    hospitalAddress = models.CharField(max_length=250)
+    hospitalPhoneNo = models.CharField(max_length=12)
+    adminId = models.ForeignKey(administrator, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hospitalId
+
+class administrator(models.Model):
+    adminId = models.AutoField(primary_key=True)
+    adminName = models.CharField(max_length= 100)
+
+    def __str__(self):
+        return self.adminId
+
+
+class nurseBed(models.Model):
+    nurseBedId = models.AutoField(primary_key=True)
+    nurseId = models.ForeignKey(nurse, related_name='nurseId')
+    bedId = models.ForeignKey(bedInfo, related_name='bedId')
+
+
+    def __str__(self):
+        return self.nurseBedId
